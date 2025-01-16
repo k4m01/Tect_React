80695142a2b681dde67fe193b1784dd0451c1263
k4m01 <152370438+k4m01@users.noreply.github.com>
2025-01-16 15:48:54 +0700

    add state-func.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import RefsFunc from './refs-func';
+import MessageBoxState from './state-func';
 
 function App() {
-  return <RefsFunc/>
+  return <MessageBoxState/>
 }
 
 export default App;
