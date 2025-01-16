405d1aab557ce6ff663a9ad68832d21a4fb4f547
k4m01 <152370438+k4m01@users.noreply.github.com>
2025-01-16 10:06:39 +0700

    add EvenButton show Alert

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import {Header,Content,Footer} from'./func-conponent';
-import Calandar from './class-component';
+import Button from './event-botton';
 
 function App() {
-  return (
-    <>
-      <Header/>
-      <p><center><Calandar/></center></p>
-      <Content/>
-      <Footer/>
-    </>
-  )
+  return <Button/>
 }
 
 export default App;
